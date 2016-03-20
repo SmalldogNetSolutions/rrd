d010ffd6a3581ba0c1291d20b047c21015424485
Chris Sutton <chris@smalldognet.com>
2016-03-20 16:07:58 -0700

    update

--- bin/getted.pl
+++ bin/getted.pl
@@ -5,32 +5,30 @@ use XML::Simple;
 use WWW::Curl::Simple;
 use Data::Dumper;
 
-my $host = $ARGV[0];
 my %type = (
 	'10.1.0.4' => "house/elecg",
-	'10.1.0.6' => "house/elech",
+	'10.1.0.5' => "house/elech",
 	);
 
-exit unless($host);
+foreach my $host (keys %type) {
 
-exit unless($type{$host});
-
-eval {
-	my $curl = WWW::Curl::Simple->new(check_ssl_certs => 0, timeout => 2);
-	my $resp = $curl->get("http://$host/api/LiveData.xml");
-	if ($resp->is_success) {
-		my $xml;
-		eval { $xml = XMLin($resp->content); };
-		if ($@) {
-			exit;
-		}
-	
-		my $watts = $xml->{'Power'}{'Total'}{'PowerNow'};
-		if ($watts) {
-			my $kw = sprintf "%.2f", $watts/1000;
-			my $rcurl = WWW::Curl::Simple->new(check_ssl_certs => 0, timeout => 10);
-			my $rresp = $rcurl->get("http://rrd.smalldognet.com/$type{$host}?kw_now=$kw");
-			print $rresp->content;
+	eval {
+		my $curl = WWW::Curl::Simple->new(check_ssl_certs => 0, timeout => 2);
+		my $resp = $curl->get("http://$host/api/LiveData.xml");
+		if ($resp->is_success) {
+			my $xml;
+			eval { $xml = XMLin($resp->content); };
+			if ($@) {
+				exit;
+			}
+		
+			my $watts = $xml->{'Power'}{'Total'}{'PowerNow'};
+			if ($watts) {
+				my $kw = sprintf "%.2f", $watts/1000;
+				my $rcurl = WWW::Curl::Simple->new(check_ssl_certs => 0, timeout => 10);
+				my $rresp = $rcurl->get("http://rrd.smalldognet.com/$type{$host}?kw_now=$kw");
+				#print $rresp->content;
+			}
 		}
 	}
 }
